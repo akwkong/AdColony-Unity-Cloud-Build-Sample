008145ab6bae14c65168f927e129370b8b53d4eb
Andrew Kong <andrew.kong@adcolony.com>
2017-05-30 10:27:53 -0700

    Test out live zones

--- ADCUnitySample/Assets/Scripts/Constants.cs
+++ ADCUnitySample/Assets/Scripts/Constants.cs
@@ -9,9 +9,9 @@ public class Constants : MonoBehaviour {
 	public static string InterstitialZoneID = "vzf8fb4670a60e4a139d01b5";
 	public static string CurrencyZoneID = "vzf8e4e97704c4445c87504e";
 #elif UNITY_ANDROID
-	public static string AppID = "app185a7e71e1714831a49ec7";
-	public static string InterstitialZoneID = "vz06e8c32a037749699e7050";
-	public static string CurrencyZoneID = "vz1fd5a8b2bf6841a0a4b826";
+	public static string AppID = "appbfa17d7d8d6f429db5";//"app185a7e71e1714831a49ec7";
+	public static string InterstitialZoneID = "vz2452b88111d64633be";//"vz06e8c32a037749699e7050";
+	public static string CurrencyZoneID = "vze6a283d2d8574bc491";//"vz1fd5a8b2bf6841a0a4b826";
 #else
 	public static string AppID = "dummyAppID";
 	public static string InterstitialZoneID = "dummyInterstitialZoneID";
